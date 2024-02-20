3a0af2183c7b9bffe59251e0bf0218ad25e01cc2
Collin <cdchamberlin.rva@gmail.com>
2024-02-19 21:58:39 -0500

    Added User Schema.

---
+++ Lab-2/models/user.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: {type: String, trim: true, required: true},
+    lastName: {type: String, trim: true, required: true},
+    email: {type: String, trim: true, required: true},
+    profilePic: {type: Blob},
+    createdAt: {type: Date, default: Date.now},
+    password: {type: String, trim: true},
+    updatedAt: {type: Date, default: Date.now},
+    alias: {type: String, trim: true}
+})
