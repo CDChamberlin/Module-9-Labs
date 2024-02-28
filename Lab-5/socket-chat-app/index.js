db181bd0eaa89c835e650250d01f73e5b01ce3f4
Collin <cdchamberlin.rva@gmail.com>
2024-02-27 20:30:57 -0500

    Updated formatting. Removed some extra console logs.

--- Lab-5/socket-chat-app/index.js
+++ Lab-5/socket-chat-app/index.js
@@ -1,36 +1,28 @@
-const express = require("express")
-const app = express()
-const http = require('http')
-const {Server} = require('socket.io')
-const { isBooleanObject } = require("util/types")
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const { isBooleanObject } = require("util/types");
 
-const server = http.createServer(app)
-const io = new Server(server)
+const server = http.createServer(app);
+const io = new Server(server);
 
-app.get('/', (req, res) =>{
-    res.sendFile(__dirname + '/public/index.html')
-})
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-io.on('connection', (socket) => {
-    
-    socket.broadcast.emit('connection', "A new user joined the chat")
+io.on("connection", (socket) => {
+  io.emit("connection", "a user connected");
 
-    console.log('SERVER - a user connected')
-    
-    socket.on('disconnect', () => {
-        socket.broadcast.emit('disconnected', "A user disconnected from the chat" )
-    })
+  socket.on("disconnect", () => {
+    io.emit("disconnection", "A user has disconnected");
+  });
 
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg)
-    })
-})
-
-// io.on('disconnection', (socket) => {
-//     socket.broadcast.emit("A user disconnected")
-// })
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
 
 server.listen(3000, () => {
-    console.log('listening on *:3000')
-})
-
+  console.log("listening on *:3000");
+});
